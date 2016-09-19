743fcba34cc60935e4fa05007a1a63116348e724
Zac Clemans <zaclem01@gmail.com>
2016-09-19 18:49:13 -0400

    add left border on mobile menu items

---
+++ style.2dfc733c2bf22dec6ab5.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([3,0],[function(n,c){}]);
+//# sourceMappingURL=style.2dfc733c2bf22dec6ab5.bundle.js.map
--- style.59b196d487aa9224ff5c.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3,0],[function(n,c){}]);
-//# sourceMappingURL=style.59b196d487aa9224ff5c.bundle.js.map
