d125472ff000202b50b946cfd4c8508030a4f266
Zac Clemans <zaclem01@gmail.com>
2016-09-19 10:02:31 -0400

    implement changes suggested in forum thread

--- style.0b588adb47efea3428e4.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3,0],[function(n,c){}]);
-//# sourceMappingURL=style.0b588adb47efea3428e4.bundle.js.map
---
+++ style.59b196d487aa9224ff5c.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([3,0],[function(n,c){}]);
+//# sourceMappingURL=style.59b196d487aa9224ff5c.bundle.js.map
