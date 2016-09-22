74017bfaa2a20e9c5bcf288859721fa0337e74f3
Zac Clemans <zaclem01@gmail.com>
2016-09-22 18:00:08 -0400

    adjust calc last time!

--- style.9467d757520b71956632.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3,0],[function(n,c){}]);
-//# sourceMappingURL=style.9467d757520b71956632.bundle.js.map
---
+++ style.bfeafa0576d3a28070f7.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([3,0],[function(n,c){}]);
+//# sourceMappingURL=style.bfeafa0576d3a28070f7.bundle.js.map
