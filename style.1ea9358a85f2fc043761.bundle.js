f63a69778d396456965ccfb55c0f5ad15b7673b7
Zac Clemans <zaclem01@gmail.com>
2016-09-22 19:06:55 -0400

    move width back on calc

---
+++ style.1ea9358a85f2fc043761.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([3,0],[function(n,c){}]);
+//# sourceMappingURL=style.1ea9358a85f2fc043761.bundle.js.map
--- style.4287df6c23b226f366df.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3,0],[function(n,c){}]);
-//# sourceMappingURL=style.4287df6c23b226f366df.bundle.js.map
