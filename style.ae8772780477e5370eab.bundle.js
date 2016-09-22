f3299eb1cb10a43cacebda028728400d72728a43
Zac Clemans <zaclem01@gmail.com>
2016-09-22 18:58:53 -0400

    increase calc demo width on mobile

--- style.ae8772780477e5370eab.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3,0],[function(n,c){}]);
-//# sourceMappingURL=style.ae8772780477e5370eab.bundle.js.map
---
+++ style.b4975d29b7a7b3b2a9d5.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([3,0],[function(n,c){}]);
+//# sourceMappingURL=style.b4975d29b7a7b3b2a9d5.bundle.js.map
