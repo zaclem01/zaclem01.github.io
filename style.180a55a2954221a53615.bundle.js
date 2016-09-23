bb77d5bbc574bdebbe8e0926d072006385c62d3c
Zac Clemans <zaclem01@gmail.com>
2016-09-23 10:38:22 -0400

    increase hero image on mobile

--- style.180a55a2954221a53615.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3,0],[function(n,c){}]);
-//# sourceMappingURL=style.180a55a2954221a53615.bundle.js.map
---
+++ style.bd9a613f7ee6bb3c58a2.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([3,0],[function(n,c){}]);
+//# sourceMappingURL=style.bd9a613f7ee6bb3c58a2.bundle.js.map
